e2abe0dac48a7a4ec9b5c21000c04c8bca7093e5
circunspecter <circunspecter@yahoo.es>
2013-10-21 16:29:31 +0200

    Being responsive

--- assets/js/mee.js
+++ assets/js/mee.js
@@ -1,5 +1,5 @@
 $(function () {
-    if($('.middle .home').length)
+    if($('.middle .home').length) // AND WIDTH IS > 767px !
     {
         $('.home .ballon img').addClass('jfixed');
         balloncen();
@@ -16,17 +16,23 @@ $(function () {
             },
             'mouseenter': function () {
                 $(this).children('a').toggleClass('jhover');
-                $(this).closest('.list')
-                .siblings('.ballon')
-                .find('img').not('.hide')
-                .toggleClass('anim anim2')
-                .prop('src', $(this).find('img').prop('src'));
+                if($('.home .ballon').is(':visible'))
+                {
+                    $(this).closest('.list')
+                    .siblings('.ballon')
+                    .find('img').not('.hide')
+                    .toggleClass('anim anim2')
+                    .prop('src', $(this).find('img').prop('src'));
+                }
             },
             'mouseleave': function () {
                 $(this).children('a').toggleClass('jhover');
-                secImg = $(this).closest('.list').siblings('.ballon').find('img').not('.hide');
-                secImg.toggleClass('anim anim2');
-                secImg.prop('src', secImg.data('oSrc'));
+                if($('.home .ballon').is(':visible'))
+                {
+                    secImg = $(this).closest('.list').siblings('.ballon').find('img').not('.hide');
+                    secImg.toggleClass('anim anim2');
+                    secImg.prop('src', secImg.data('oSrc'));
+                }
             }
         }).find('span').css('cursor', 'pointer');
     }
@@ -34,25 +40,28 @@ $(function () {
 
 function balloncen()
 {
-    var ballonTop = ($( window ).height() / 2);
-
-    var pHeader = $(window).scrollTop() > 126
-                ? 0
-                : (126 - $(window).scrollTop()) / 2 ;
-
-    var pFooter = ( $(window).height() + $(window).scrollTop() >=  $('body').height() - 135 )
-                ? ($('body').height() - 135 - ($(window).height() + $(window).scrollTop())) / 2
-                : 0 ;
-
-    $('.home .ballon img').css('top', ballonTop + pHeader + pFooter);
-
-    if(
-        (pHeader > 0 && $('.home .ballon img').eq(0).hasClass('hide'))
-        ||
-        (pHeader == 0 && pFooter != 0 && $('.home .ballon img').eq(1).hasClass('hide'))
-    )
+    if($('.home .ballon').is(':visible'))
     {
-        $('.home .ballon img').toggleClass('hide');
+        var ballonTop = ($( window ).height() / 2);
+
+        var pHeader = $(window).scrollTop() > 126
+                    ? 0
+                    : (126 - $(window).scrollTop()) / 2 ;
+
+        var pFooter = ( $(window).height() + $(window).scrollTop() >=  $('body').height() - 135 )
+                    ? ($('body').height() - 135 - ($(window).height() + $(window).scrollTop())) / 2
+                    : 0 ;
+
+        $('.home .ballon img').css('top', ballonTop + pHeader + pFooter);
+
+        if(
+            (pHeader > 0 && $('.home .ballon img').eq(0).hasClass('hide'))
+            ||
+            (pHeader == 0 && pFooter != 0 && $('.home .ballon img').eq(1).hasClass('hide'))
+        )
+        {
+            $('.home .ballon img').toggleClass('hide');
+        }
     }
 }
 
